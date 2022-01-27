0cc58441afadd5fb4730f1c5acbea628fc97f722
Tørres Båshus <89388357+Thyrgils@users.noreply.github.com>
2022-01-27 21:50:22 +0100

    Create myquote.go
    myquote.go skapt av myquote.go fra modul1del2

---
+++ myquote.go
@@ -0,0 +1,19 @@
+package myquote
+import(
+
+	"rsc.io/quote"
+	"fmt"
+
+)
+func Myglass(){
+	fmt.Println (quote.Glass())
+}
+func Mygo(){
+	fmt.Println (quote.Go())
+}
+func Myhello(){
+	fmt.Println (quote.Hello())
+}
+func Myopt(){
+	fmt.Println (quote.Opt())
+}
